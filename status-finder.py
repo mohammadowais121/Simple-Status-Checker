38b2ee8fd4a3b3cb7a801a63eac158b31472acbc
mohammadowais121 <46589812+mohammadowais121@users.noreply.github.com>
2020-03-13 17:30:38 +0530

    Add files via upload

---
+++ status-finder.py
@@ -0,0 +1,38 @@
+'''
+Author: Mohammad Owais
+Email: mohammadowais616@gmail.com
+How to use:
+
+URLS.TXT should contain HTTP/HTTPS URLS.
+python status-finder
+or
+python3 status-finder
+'''
+
+import requests
+import os,sys
+import sys
+import ssl
+from OpenSSL import SSL
+
+def main():
+	
+	print('\033[32m' + """
+ ___   _         _                    ___            _            
+(  _`\( )_      ( )_                /'____          ( )           
+| (_(_| ,_)  _ _| ,_)_   _  ___    | (__(_) ___    _| |  __  _ __ 
+`\__ \| |  /'_` | | ( ) ( /',__)   | ,__| /' _ `\/'_` |/'__`( '__)
+( )_) | |_( (_| | |_| (_) \__, \   | |  | | ( ) ( (_| (  ___| |   
+`\____`\__`\__,_`\__`\___/(____/   (_)  (_(_) (_`\__,_`\____(_)   
+                                                                  
+                                                                  """)
+	print('\033[32m' +"""                                                       Coded by Mohamad Owais""")
+	print("[~] URLS|Status..")
+	for link in open("urls.txt","r").read().splitlines():
+		try:
+			r = requests.get(link, allow_redirects=True, timeout=4)
+			print(link + " ["+ str(r.status_code)+ "]")
+		except requests.exceptions.SSLError:
+			print("Host Dead Probably")
+if __name__=="__main__":
+	main()
